316eb5cb2adb97121274ea9c8c0f0ce6c1836110
pankajshindegithub <pankajshinde2434@gmail.com>
2025-08-31 12:46:20 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById("hamburger");
+      const sidebar = document.getElementById("sidebar");
+
+      hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active"); 
+        sidebar.classList.toggle("active"); 
+      });
+  
